16580f7d6b063555e46fa438e092e013d7f5817a
Rafael Moura de Almeida <rafael.moura.almeida@usp.br>
2024-12-09 19:50:36 -0300

    refactor: refactored project structure
    added ganimedes_authentication_service as a micro-service in java
    resposible for create and validade tokens
    
    moved dotnet backend to a new folder called 'ganimedes_backend'

--- Sistema-Ganimes.Back.Tests/Application/FormularioControllerTests.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Sistema_Ganimes.Back.Tests.Application
-{
-    public class FormularioControllerTests
-    {
-    }
-}
---
+++ ganimedes_backend/Sistema-Ganimedes.Infrastructure/Repository/UsuarioRepository.cs
@@ -0,0 +1,67 @@
+﻿using Sistema_Ganimedes.Domain.Entities;
+using Sistema_Ganimedes.Infrastructure.Common.Persistence;
+using Sistema_Ganimedes.Infrastructure.Interfaces;
+using Dapper;
+using Sistema_Ganimedes.Domain.Scripts;
+using Npgsql;
+using System.Globalization;
+
+namespace Sistema_Ganimedes.Infrastructure.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public UsuarioRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int CadastrarAluno(Aluno aluno)
+        {
+            var connection = _dbContext.GetConnection();
+
+            try
+            {
+                var rowsAffected = connection.Execute(UsuarioScripts.CreateAluno(), aluno);
+                connection.Close();
+                return rowsAffected;
+            }
+            catch (NpgsqlException)
+            {
+                connection.Close();
+                throw;
+            }
+        }
+
+        public int CadastrarUsuario(Usuario usuario)
+        {
+            var connection = _dbContext.GetConnection();
+
+            try
+            {
+                var rowsAffected = connection.Execute(UsuarioScripts.CreateUsuario(), usuario);
+                connection.Close();
+                return rowsAffected;
+            } catch(NpgsqlException)
+            {
+                connection.Close();
+                throw;
+            }
+
+
+        }
+
+        public Usuario? GetUsuario(string nUsp)
+        {
+
+            var connection = _dbContext.GetConnection();
+
+            var usuario = connection.QueryFirstOrDefault<Usuario>(UsuarioScripts.GetUsuario(nUsp));
+
+            connection!.Close();
+
+            return usuario;
+        }
+    }
+}
